c53449f222060b89d280af884bd5297af384dd35
MRaoShivani <raoshivani323@gmail.com>
2024-02-24 00:32:00 +0530

    added 3 new model

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("All machine learning model codes will be added here")
+
+def model():
+    print("adding code for ML model 1")
+
+def model2():
+    print("adding code for ML model 2")
+
+def model3():
+    print("adding code for ML model 3")
