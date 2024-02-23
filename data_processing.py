f4a4db2917adfec4a0e81f822eaee88dfbf563c3
MRaoShivani <raoshivani323@gmail.com>
2024-02-23 23:31:38 +0530

    Adding sum function

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,7 @@
 print('Adding all the data processing code related to fraud detection')
-print('adding new commit')
+def sum(a,b):
+    c =a+b
+    return c
+
+res = sum(3,4)
+print(f' the result is {res}')
